9a98bdc5dc042e32a54dca594a3054b3af4fb29c
Matias Bravo <mbravo@localhost.localdomain>
2019-12-10 06:46:29 -0300

    Fixed missing library

--- data_func.py
+++ data_func.py
@@ -137,8 +137,11 @@ def lsst(in_path,out_path):
 	# Reading FITS
 	####################
 	fits_list=glob.glob(f'{in_path}*.fit')
-	pool=mp.Pool(processes=min(56,len(fits_list)))
-	csv_out=[pool.apply_async(fits2csv,(f,out_path,)) for f in fits_list]
-	csv_out=[c.get() for c in csv_out]
+	#pool=mp.Pool(processes=min(56,len(fits_list)))
+	#csv_out=[pool.apply_async(fits2csv,(f,out_path,)) for f in fits_list]
+	#csv_out=[c.get() for c in csv_out]
+	csv_out=[fits2csv(f,out_path) for f in fits_list]
+	for c in csv_out:
+		print(c)
 	
 	return(csv_out)
