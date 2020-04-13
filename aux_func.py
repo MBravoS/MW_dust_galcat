c4da33b92d86ec593f958573a74117c03187deb9
Matias Bravo <mbravo@localhost.localdomain>
2020-04-13 06:39:19 -0400

    ctm

--- aux_func.py
+++ aux_func.py
@@ -201,7 +201,7 @@ def pix_stat(fname,nside,bsel,b1,b2,mcut,b1cut,b2cut,zr,bcheck):
 				pixel_df[f'{nside_key}_{z_key}_count']=counts
 				pixel_df[f'{nside_key}_{z_key}_mag']=mag
 				pixel_df[f'{nside_key}_{z_key}_col']=col
-				print(type(bin_id),type(ebv),type(counts),type(mag),type(col))
+				print(bin_id.shape,ebv.shape,counts.shape,mag.shape,col.shape)
 				pname=fname.replace('galaxies',f'pixel_{nside_key}')
 				#print('Array check:')
 				#for k in pixel_df.keys():
