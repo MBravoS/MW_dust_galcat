24ec1a2b203957c3650c9163d89d094e3ed27d63
Matias Bravo <mbravo@localhost.localdomain>
2020-04-14 09:07:02 -0400

    wea

--- aux_func.py
+++ aux_func.py
@@ -203,7 +203,7 @@ def pix_stat(fname,nside,bsel,b1,b2,mcut,b1cut,b2cut,zr,bcheck):
 				pixel_df[f'{nside_key}_{z_key}_mag']=mag
 				pixel_df[f'{nside_key}_{z_key}_col']=col
 				pname=fname.replace('galaxies',f'pixel_{nside_key}')
-				print(pixel_df)
+				print(pixel_df[f'{nside_key}_EBV'])
 				pd.DataFrame(pixel_df).to_csv(pname,index=False)
 				pixel_name.append(pname)
 			else:
