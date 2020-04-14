6c05d21450e86a324242d1dcf18e926bea5f3fa8
Matias Bravo <mbravo@localhost.localdomain>
2020-04-14 11:54:19 -0400

    wea

--- geryon_lsst_deep.sh
+++ geryon_lsst_deep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -V
-#PBS -N MWdust_LSST
+#PBS -N MWdust_LSST_deep
 #PBS -k eo
 #PBS -l nodes=1:ppn=56
-#PBS -l walltime=24:00:00
+#PBS -l walltime=48:00:00
 #PBS -q newton
 
 ##########################
