e40fa4a138fe171999eaf7c2812066882f5fb80c
Matias Bravo <mbravo@localhost.localdomain>
2020-07-15 07:15:03 -0400

    Fine-tuning plots

--- lc_description_plots.py
+++ lc_description_plots.py
@@ -50,7 +50,7 @@ for i in np.linspace(0,1,5):
 # col-mag (ap)
 ####################
 fig=plot.figure(figsize=(new_fig_size[0],new_fig_size[0]*2))
-spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.08,right=0.97,bottom=0.04,top=0.99)
+spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.1,right=0.98,bottom=0.06,top=0.99)
 fax=[fig.add_subplot(spec[0,0]),fig.add_subplot(spec[1,0]),fig.add_subplot(spec[2,0])]
 temp=fax[0].get_xaxis().set_ticklabels([])
 temp=fax[1].get_xaxis().set_ticklabels([])
@@ -73,9 +73,13 @@ for zr in zrange:
 		sp.contourp(temp['u_ap_nodust'],temp['u_ap_nodust']-temp['z_ap_nodust'],bins=[np.linspace(19,28,61),np.linspace(-1,7,61)],ax=fax[2],
 					xinvert=True,smooth=0.8,filled=True,colors=colour_list[j],plabel=False,xlabel='$u$ [mag]',ylabel='$u-z$ [mag]')
 	j+=1
-fax[0].text(22,6,'\\textsc{GALFORM}$_{r<24.8}$')
-fax[1].text(22,6,'\\textsc{Buzzard}$_{r<24.8}$')
-fax[2].text(22,6,'\\textsc{Buzzard}$_{r<26.0}$')
+#Legend
+fax[0].legend([patches.Patch(color=col_map(0.0/(L-0.8)),alpha=0.8),patches.Patch(color=col_map(2.0/(L-0.8)),alpha=0.8),
+				patches.Patch(color=col_map(4.0/(L-0.8)),alpha=0.8)],
+				['$z_{0.0,0.3}$','$z_{0.6,0.9}$','$z_{1.2,2.5}$'],fontsize=17,loc=2)
+fax[0].text(21.8,6.2,'\\textsc{GALFORM}$_{r<24.8}$')
+fax[1].text(21.8,6.2,'\\textsc{Buzzard}$_{r<24.8}$')
+fax[2].text(21.8,6.2,'\\textsc{Buzzard}$_{r<26.0}$')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/mag_col_ap.pdf')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/mag_col_ap.png')
 plot.close()
@@ -84,7 +88,7 @@ plot.close()
 # col-mag (ab)
 ####################
 fig=plot.figure(figsize=(new_fig_size[0],new_fig_size[0]*2))
-spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.08,right=0.97,bottom=0.04,top=0.99)
+spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.14,right=0.98,bottom=0.06,top=0.99)
 fax=[fig.add_subplot(spec[0,0]),fig.add_subplot(spec[1,0]),fig.add_subplot(spec[2,0])]
 temp=fax[0].get_xaxis().set_ticklabels([])
 temp=fax[1].get_xaxis().set_ticklabels([])
@@ -110,9 +114,13 @@ for zr in zrange:
 					ax=fax[2],xinvert=True,smooth=0.8,filled=True,colors=colour_list[j],plabel=False,
 					xlabel='$r_\mathrm{ab}$ [mag]',ylabel='${g-r}_\mathrm{ab}$ [mag]')
 	j+=1
-fax[0].text(-12.5,-0.6,'\\textsc{GALFORM}$_{r<24.8}$')
-fax[1].text(-12.5,-0.6,'\\textsc{Buzzard}$_{r<24.8}$')
-fax[2].text(-12.5,-0.6,'\\textsc{Buzzard}$_{r<26.0}$')
+#Legend
+fax[0].legend([patches.Patch(color=col_map(0.0/(L-0.8)),alpha=0.8),patches.Patch(color=col_map(2.0/(L-0.8)),alpha=0.8),
+				patches.Patch(color=col_map(4.0/(L-0.8)),alpha=0.8)],
+				['$z_{0.0,0.3}$','$z_{0.6,0.9}$','$z_{1.2,2.5}$'],fontsize=17,loc=2)
+fax[0].text(-12.5,-0.15,'\\textsc{GALFORM}$_{r<24.8}$')
+fax[1].text(-12.5,-0.15,'\\textsc{Buzzard}$_{r<24.8}$')
+fax[2].text(-12.5,-0.15,'\\textsc{Buzzard}$_{r<26.0}$')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/mag_col_ab.pdf')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/mag_col_ab.png')
 plot.close()
@@ -121,7 +129,7 @@ plot.close()
 # col-col
 ####################
 fig=plot.figure(figsize=(new_fig_size[0],new_fig_size[0]*2))
-spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.12,right=0.97,bottom=0.04,top=0.98)
+spec=gs.GridSpec(nrows=3,ncols=1,figure=fig,wspace=0,hspace=0,left=0.12,right=0.97,bottom=0.06,top=0.98)
 fax=[fig.add_subplot(spec[0,0]),fig.add_subplot(spec[1,0]),fig.add_subplot(spec[2,0])]
 temp=fax[0].get_xaxis().set_ticklabels([])
 temp=fax[1].get_xaxis().set_ticklabels([])
@@ -133,13 +141,14 @@ for i in range(3):
 	fax[i].set_rasterized(True)
 temp=Buz260_data.loc[(Buz260_data['zobs_sim']>zrange[0][0])&(Buz260_data['zobs_sim']<zrange[0][1])]
 sp.hist2D(temp['r_ab']-temp['z_ab'],temp['u_ab']-temp['r_ab'],bins=[np.linspace(-1,1.5,61),np.linspace(-1,3.5,61)],
-			ax=fax[0],cmap=cmo.cm.gray_r,clog=False,ylabel='${u-r}_\mathrm{ab}$ [mag]')
+			ax=fax[0],cmap=cmo.cm.speed,clog=False,ylabel='${u-r}_\mathrm{ab}$ [mag]',clabel='PDF')
 temp=Buz260_data.loc[(Buz260_data['zobs_sim']>zrange[2][0])&(Buz260_data['zobs_sim']<zrange[2][1])]
 sp.hist2D(temp['r_ab']-temp['z_ab'],temp['u_ab']-temp['r_ab'],bins=[np.linspace(-1,1.5,61),np.linspace(-1,3.5,61)],
-			ax=fax[1],cmap=cmo.cm.gray_r,ylabel='${u-r}_\mathrm{ab}$ [mag]')
+			ax=fax[1],cmap=cmo.cm.speed,ylabel='${u-r}_\mathrm{ab}$ [mag]',clabel='PDF')
 temp=Buz260_data.loc[(Buz260_data['zobs_sim']>zrange[4][0])&(Buz260_data['zobs_sim']<zrange[4][1])]
 sp.hist2D(temp['r_ab']-temp['z_ab'],temp['u_ab']-temp['r_ab'],bins=[np.linspace(-1,1.5,61),np.linspace(-1,3.5,61)],
-			ax=fax[2],cmap=cmo.cm.gray_r,xlabel='${r-z}_\mathrm{ab}$ [mag]',ylabel='${u-r}_\mathrm{ab}$ [mag]')
+			ax=fax[2],cmap=cmo.cm.speed,xlabel='${r-z}_\mathrm{ab}$ [mag]',
+			ylabel='${u-r}_\mathrm{ab}$ [mag]',clabel='PDF')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/col_col.pdf')
 plot.savefig('/fast_scratch2/mbravo/MWdust_plots/col_col.png')
 plot.close()
